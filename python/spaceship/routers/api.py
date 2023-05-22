e51eb92f3b4921b66820ce1c8ddc84bd289d1ccd
GhostDolphin <d.ghost2ac@gmail.com>
2023-05-22 19:20:27 +0300

    Added matrix

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -6,3 +6,13 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+def matrix() -> dict:
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+    product = np.dot(matrix_a, matrix_b)
+    result = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
+    return result
